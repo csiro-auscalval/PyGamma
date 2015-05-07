db2fc3448e396dbeae686e31e04954d110188c04
Sarah Lawrie <Sarah.Lawrie@ga.gov.au>
2015-05-07 14:05:14 +1000

    update scripts for NCI processing

--- process_PALSAR_L1_SLC.bash
+++ process_PALSAR_L1_SLC.bash
@@ -63,7 +63,7 @@ cd $proj_dir/$track_dir
 ## Insert scene details top of NCI .e file
 echo "" 1>&2 # adds spaces at top so scene details are clear
 echo "" 1>&2
-echo "PROCESSING_PROJECT: "$project $track_dir $scene 1>&2
+echo "PROCESSING_PROJECT: "$project $track_dir $scene $slc_rlks"rlks" $slc_alks"alks" 1>&2
 
 ## Copy output of Gamma programs to log files
 GM()
