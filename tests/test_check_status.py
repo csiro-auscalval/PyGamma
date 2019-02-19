20c046d2c541eed076a53ed8d67fd5e002b5cb2d
Passang Dorji <pd1813@vdi-n10.novalocal>
2019-02-19 17:24:28 +1100

    additional test cases are added

--- tests/test_check_status.py
+++ tests/test_check_status.py
@@ -4,20 +4,12 @@ import os
 import shutil
 import tempfile
 import unittest
-import mock
-import time
-import json
 from os.path import join as pjoin, abspath, dirname, exists, getmtime
 from python_scripts import check_status
+from python_scripts.constant import Wildcards, MatchStrings, FolderNames,  ErrorMessages
 from data import data as test_data
 
-from python_scripts.proc_template import PROC_FILE_TEMPLATE
-from python_scripts.initialize_proc_file import get_path
-
 DATA_DIR = pjoin(dirname(abspath(__file__)), 'data')
-
-TEST_VARS = {'safe_folder': 'S1A_IW_SLC__1SDV_20170712T192147_20170712T192214_017442_01D25D_FB1F.SAFE',
-             'tiff_file': 's1a-iw1-slc-vv-20170712t192149-20170712t192214-017442-01d25d-004.tiff'}
 
 
 class TestCheckStatus(unittest.TestCase):
@@ -31,8 +23,25 @@ class TestCheckStatus(unittest.TestCase):
         if exists(self.test_dir):
             shutil.rmtree(self.test_dir)
 
+    @staticmethod
+    def write_dummy_file(filename):
+        with open(filename, 'w') as fid:
+            fid.writelines('This is dummy text')
+
+    @classmethod
+    def create_files(cls, scene_dir, files_lists):
+        """ creates folders 'scene_dir' and files within the files_list"""
+        with open(files_lists, 'r') as src:
+            if not exists(scene_dir):
+                os.makedirs(scene_dir)
+            for item in src.readlines():
+                filename = pjoin(scene_dir, item.rstrip())
+                cls.write_dummy_file(filename)
+
     def test_checkrawdata(self):
-        """ test to check if raw checkrawdata function's execution is as expected"""
+        """ test to check if raw checkrawdata function's execution is as expected """
+        
+        #TODO make temporary zipped file with tiff datasets as source s1 data for testing
 
         kwargs = {'raw_data_path': pjoin(DATA_DIR, 'SLC_DATA_RAW'),
                   's1_dir_path': pjoin(DATA_DIR, 'SLC_DATA_SOURCE'),
@@ -41,23 +50,54 @@ class TestCheckStatus(unittest.TestCase):
 
         status = check_status.checkrawdata(**kwargs)
         self.assertTrue(status)
-        raw_data_dir = pjoin(self.test_dir, '20170712', TEST_VARS['safe_folder'], 'measurement')
 
+        raw_data_dir = pjoin(self.test_dir, test_data.TEST_VARS['tiff_folder'])
         os.makedirs(raw_data_dir)
-        with open(pjoin(raw_data_dir, TEST_VARS['tiff_file']), 'w') as fid:
-            fid.writelines('This is dummy text')
+        self.write_dummy_file(pjoin(raw_data_dir, test_data.TEST_VARS['tiff_file']))
 
         kwargs['raw_data_path'] = self.test_dir
         status = check_status.checkrawdata(**kwargs)
         self.assertTrue(status)
 
     def test_checkgammadem(self):
-        pass
+        """ test to check checkgammadem function """
+
+        kwargs = {'track': 'T111A',
+                  'gamma_dem_path': self.test_dir}
+
+        gamma_dem_files = [Wildcards.TRACK_DEM_TYPE.value.format(track=kwargs['track']),
+                           Wildcards.TRACK_DEM_PAR_TYPE.value.format(track=kwargs['track'])]
+
+        for item in gamma_dem_files:
+            filename = pjoin(self.test_dir, item)
+            self.write_dummy_file(filename)
+
+        status = check_status.checkgammadem(**kwargs)
+        self.assertTrue(status)
+        os.remove(pjoin(self.test_dir, gamma_dem_files[0]))
+        status = check_status.checkgammadem(**kwargs)
+        self.assertFalse(status)
 
     def test_checkfullslc(self):
-        pass
+        """ test to check checkfullslc function """
+
+        scene_dir = pjoin(self.test_dir, '20170712')
+        files_list = pjoin(DATA_DIR, test_data.TEST_VARS['slc_file'])
+        self.create_files(scene_dir, files_list)
+        status = check_status.checkfullslc(self.test_dir)
+        self.assertFalse(status)
+
+        os.remove(pjoin(scene_dir, Wildcards.SLC_ERROR_LOG_TYPE.value))
+        error_log = pjoin(pjoin(scene_dir, Wildcards.SLC_ERROR_LOG_TYPE.value))
+        with open(error_log, 'w'):
+            pass
+        status = check_status.checkfullslc(self.test_dir)
+        self.assertTrue(status)
 
     def test_checkmultilook(self):
+        """ test to check checkmultilook function """
+
+
         pass
 
     def test_checkbaseline(self):
--- tests/test_cleanup.py
+++ tests/test_cleanup.py
@@ -4,10 +4,11 @@ import shutil
 import tempfile
 import unittest
 import os
-from os.path import join as pjoin, abspath, dirname, exists, getmtime
+from os.path import join as pjoin, abspath, dirname, exists
 from python_scripts import clean_up
 from data import data as test_data
 import time
+import test_check_status as checkstatus
 
 DATA_DIR = pjoin(dirname(abspath(__file__)), 'data')
 
@@ -25,22 +26,19 @@ class TestCleanUp(unittest.TestCase):
 
     def test_clean_rawdatadir(self):
         """ test case to check if raw data directory is removed"""
+
         dummy_file_lists = ['dummy_{i}.txt'.format(i=idx) for idx in range(5)]
         for f in dummy_file_lists:
-            with open(pjoin(self.test_dir, f), 'w') as fid:
-                fid.writelines('This is dummy text')
-
+            checkstatus.TestCheckStatus.write_dummy_file(pjoin(self.test_dir, f))
         clean_up.clean_rawdatadir(raw_data_path=self.test_dir)
         self.assertFalse(os._exists(self.test_dir))
 
     def test_clean_scldir(self):
         """ test case to check if SLC directory is cleaned as expected"""
-        with open(pjoin(DATA_DIR, 'slc_files.txt'), 'r') as src:
-            scene_dir = pjoin(self.test_dir, '20161122')
-            os.makedirs(scene_dir)
-            for item in src.readlines():
-                with open(pjoin(scene_dir, item.rstrip()), 'w') as fid:
-                    fid.writelines('This is dummy text')
+
+        scene_dir = pjoin(self.test_dir, '20161122')
+        files_list = pjoin(DATA_DIR, test_data.TEST_VARS['slc_file'])
+        checkstatus.TestCheckStatus.create_files(scene_dir, files_list)
 
         clean_up.clean_slcdir(slc_path=self.test_dir)
         files_cleanup = [item for item in os.listdir(scene_dir)]
@@ -49,12 +47,10 @@ class TestCleanUp(unittest.TestCase):
 
     def test_clean_ifgdir(self):
         """ test case to check if IFG directory is cleaned as expected"""
-        with open(pjoin(DATA_DIR, 'ifg_files.txt'), 'r') as src:
-            scene_dir = pjoin(self.test_dir, '20161005-20161017')
-            os.makedirs(scene_dir)
-            for item in src.readlines():
-                with open(pjoin(scene_dir, item.rstrip()), 'w') as fid:
-                    fid.writelines('This is dummy text')
+
+        scene_dir = pjoin(self.test_dir, '20161005-20161017')
+        files_list = pjoin(DATA_DIR, test_data.TEST_VARS['ifg_file'])
+        checkstatus.TestCheckStatus.create_files(scene_dir, files_list)
 
         clean_up.clean_ifgdir(ifg_path=self.test_dir)
         files_cleanup = [item for item in os.listdir(scene_dir)]
@@ -63,10 +59,9 @@ class TestCleanUp(unittest.TestCase):
 
     def test_clean_demdir(self):
         """ test case to check if DEM directory is cleaned as expected"""
-        with open(pjoin(DATA_DIR, 'dem_files.txt'), 'r') as src:
-            for item in src.readlines():
-                with open(pjoin(self.test_dir, item.rstrip()), 'w') as fid:
-                    fid.writelines('This is dummy text')
+
+        files_list = pjoin(DATA_DIR, test_data.TEST_VARS['dem_file'])
+        checkstatus.TestCheckStatus.create_files(self.test_dir, files_list)
 
         clean_up.clean_demdir(dem_path=self.test_dir)
         files_cleanup = [item for item in os.listdir(self.test_dir)]
@@ -75,18 +70,16 @@ class TestCleanUp(unittest.TestCase):
 
     def test_clean_checkpoints(self):
         """ test case to check if check point file are cleaned as expected"""
-        with open(pjoin(DATA_DIR, 'check_files.txt'), 'r') as src:
+
+        with open(pjoin(DATA_DIR, test_data.TEST_VARS['check_file']), 'r') as src:
             for item in src.readlines():
-                with open(pjoin(self.test_dir, item.rstrip()), 'w') as fid:
-                    fid.writelines('This is dummy text')
+                checkstatus.TestCheckStatus.write_dummy_file(pjoin(self.test_dir, item.rstrip()))
                 time.sleep(.001)
 
         clean_up.clean_checkpoints(checkpoint_path=self.test_dir)
         files_cleanup = [item for item in os.listdir(self.test_dir)]
-        files_test = 'Slc_resize_status_logs.out'
-        if files_test not in files_cleanup:
-            flag = True
-        self.assertTrue(flag)
+        files_test = test_data.CHECKPOINT_FILES[-1]
+        self.assertTrue(files_test not in files_cleanup)
 
 
 if __name__ == '__main__':
