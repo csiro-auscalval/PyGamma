a2b51a143853175cec00b7f36a0b24ef64ffabcb
Passang Dorji <passang.dorji@ga.gov.au>
2019-12-06 10:49:56 +1100

    adding tab_names methods and refactoring to tab parameter names

--- python_scripts/process_s1_slc.py
+++ python_scripts/process_s1_slc.py
@@ -2,13 +2,15 @@
 
 import os
 import re
+from collections import namedtuple
+from typing import Optional
 import tempfile
 import logging
 from pathlib import Path
 import shutil
 import datetime
 import pandas as pd
-from subprocess_utils import working_directory, run_command
+from python_scripts.subprocess_utils import working_directory, run_command
 
 
 _LOG = logging.getLogger(__name__)
@@ -21,31 +23,47 @@ class SlcProcess:
         slc_output_dir: Path,
         polarization: str,
         scene_date: str,
-        burst_data: Path
+        burst_data: Path,
+        ref_master_tab: Optional[Path] = None,
     ):
-        self.raw_data_dir = raw_data_dir
-        self.output_dir = slc_output_dir
+        self.raw_data_dir = Path(raw_data_dir)
+        self.output_dir = Path(slc_output_dir)
         self.polarization = polarization
         self.scene_date = scene_date
         self.burst_data = burst_data
+        self.ref_master_tab = ref_master_tab
         self.raw_files_patterns = {
             "data": "*measurement/s1*-iw{swath}-slc-{polarization}*.tiff",
             "annotation": "*annotation/s1*-iw{swath}-slc-{polarization}*.xml",
             "calibration": "*annotation/calibration/calibration-s1*-iw{swath}-slc-{polarization}*.xml",
             "noise": "*annotation/calibration/noise-s1*-iw{swath}-slc-{polarization}*.xml",
-            "orbit_file": "*.EOF"
+            "orbit_file": "*.EOF",
         }
         self.phase_shift_date = datetime.date(2015, 3, 15)
-        self.swath_slc_par = "{}_{}_iw{}.slc.par"
-        self.swath_slc_tops_par = "{}_{}_iw{}.slc.TOPS_par"
-        self.swath_slc = "{}_{}_iw{}.slc"
         self.slc_tabs_params = None
         self.slc_prefix = None
         self.acquisition_date = None
         self.slc_tab = None
-        self.slc = None
-        self.slc_par = None
         self.orbit_file = None
+        self.temp_slc = []
+
+    def swath_tab_names(self, swath, pre_fix):
+        swath_par = "{}_{}_iw{}.slc.par"
+        swath_tops_par = "{}_{}_iw{}.slc.TOPS_par"
+        swath_slc = "{}_{}_iw{}.slc"
+        swath_tab = namedtuple('swath_tab', ['slc', 'par', 'tops_par'])
+        return swath_tab(swath_slc.format(pre_fix, self.polarization.upper(), swath),
+                         swath_par.format(pre_fix, self.polarization.upper(), swath),
+                         swath_tops_par.format(pre_fix, self.polarization.upper(), swath))
+
+    def slc_tab_names(self, pre_fix):
+        _slc = "{}_{}.slc"
+        slc_par = "{}_{}.slc.par"
+        slc_tops_par = "{}_{}.slc.TOPS_par"
+        slc_tab = namedtuple('slc_tab', ['slc', 'par', 'tops_par'])
+        return slc_tab(_slc.format(pre_fix, self.polarization.upper()),
+                       slc_par.format(pre_fix, self.polarization.upper()),
+                       slc_tops_par.format(pre_fix, self.polarization.upper()))
 
     def slc_safe_files(self):
         safe_files = [
@@ -67,24 +85,20 @@ class SlcProcess:
 
             for swath in [1, 2, 3]:
                 _concat_tabs[_id][swath] = dict()
+                tab_names = self.swath_tab_names(swath, _id)
                 raw_files = [
                     list(
                         save_file.glob(
-                            val.format(swath=swath, polarization=self.polarization)
+                            val.format(swath=swath, polarization=self.polarization.lower())
                         )
                     )[0].as_posix()
                     for key, val in self.raw_files_patterns.items()
                 ]
-                _slc = self.swath_slc.format(_id, self.polarization.upper(), swath)
-                _par = self.swath_slc_par.format(_id, self.polarization.upper(), swath)
-                _tops_par = self.swath_slc_tops_par.format(
-                    _id, self.polarization.upper(), swath
-                )
 
                 # collect the variables needed to perform slc processing
-                _concat_tabs[_id][swath]["slc"] = _slc
-                _concat_tabs[_id][swath]["par"] = _par
-                _concat_tabs[_id][swath]["tops_par"] = _tops_par
+                _concat_tabs[_id][swath]["slc"] = tab_names.slc
+                _concat_tabs[_id][swath]["par"] = tab_names.par
+                _concat_tabs[_id][swath]["tops_par"] = tab_names.tops_par
 
                 command = [
                     "par_S1_SLC",
@@ -92,9 +106,9 @@ class SlcProcess:
                     raw_files[1],
                     raw_files[2],
                     raw_files[3],
-                    _par,
-                    _slc,
-                    _tops_par,
+                    tab_names.par,
+                    tab_names.slc,
+                    tab_names.tops_par,
                     "0",
                     "-",
                     "-",
@@ -103,9 +117,13 @@ class SlcProcess:
 
                 # assign orbit file name
                 self.orbit_file = raw_files[4]
+
+                # store names of flies to be removed later
+                for item in [tab_names.slc, tab_names.par, tab_names.tops_par]:
+                    self.temp_slc.append(item)
         self.slc_tabs_params = _concat_tabs
 
-    def _write_tabs(self, slc_tab_file, tab_params=None, _id=None):
+    def _write_tabs(self, slc_tab_file, tab_params=None, _id=None, slc_data_dir=None):
         if slc_tab_file.exists():
             _LOG.info(
                 f"{slc_tab_file.name} exits, skipping writing of slc tab parameters"
@@ -115,26 +133,25 @@ class SlcProcess:
         with open(slc_tab_file, "w") as fid:
             for swath in [1, 2, 3]:
                 if tab_params is None:
-                    _slc = self.swath_slc.format(_id, self.polarization.upper(), swath)
-                    _par = self.swath_slc_par.format(
-                        _id, self.polarization.upper(), swath
-                    )
-                    _tops_par = self.swath_slc_tops_par.format(
-                        _id, self.polarization.upper(), swath
-                    )
+                    tab_names = self.swath_tab_names(swath, _id)
+                    _slc = tab_names.slc
+                    _par = tab_names.par
+                    _tops_par = tab_names.tops_par
                 else:
                     _slc = tab_params[swath]["slc"]
                     _par = tab_params[swath]["par"]
                     _tops_par = tab_params[swath]["tops_par"]
+
+                if slc_data_dir is not None:
+                    _slc = slc_data_dir.joinpath(_slc).as_posix()
+                    _par = slc_data_dir.joinpath(_par).as_posix()
+                    _tops_par = slc_data_dir.joinpath(_tops_par).as_posix()
+
                 fid.write(_slc + " " + _par + " " + _tops_par + "\n")
 
-
     def concatenate(self):
-
         with tempfile.TemporaryDirectory() as tmpdir:
-            tmpdir = Path(
-                "/g/data/u46/users/pd1813/INSAR/INSAR_BACKSCATTER/test_backscatter_workflow/temp_dir"
-            )
+            tmpdir = Path(tmpdir)
             start_tabs, *rest_tabs = sorted(
                 self.slc_tabs_params.items(), key=lambda x: x[1]["datetime"]
             )
@@ -175,6 +192,10 @@ class SlcProcess:
                 # assign slc_tab3 to slc_tab1 to perform series of concatenation
                 slc_tab1 = slc_tab3
 
+            # clean up the temporary slc files after clean up
+            for fp in self.temp_slc:
+                os.remove(fp)
+
             # prefix for final slc
             self.slc_prefix = slc_prefix
 
@@ -186,91 +207,212 @@ class SlcProcess:
                 ),
             )
 
-    def phase_shift(self):
-        # perform phase shift correction for IW1 scene is acquired before 15th March, 2015
+    def phase_shift(self, swath=1):
+        """perform phase shift correction for IW1 scenes acquired before 15th March, 2015"""
+
         with tempfile.TemporaryDirectory() as tmpdir:
             tmpdir = Path(tmpdir)
-
             if self.acquisition_date < self.phase_shift_date:
                 slc_dir = Path(os.getcwd())
-                iw1_slc = self.swath_slc.format(self.slc_prefix, self.polarization.upper(), 1)
-                iw1_slc_par = self.swath_slc_par.format(
-                    self.slc_prefix, self.polarization.upper(), 1
-                )
+                tab_names = self.swath_tab_names(swath, self.slc_prefix)
                 command = [
                     "SLC_phase_shift",
-                    slc_dir.joinpath(iw1_slc).as_posix(),
-                    slc_dir.joinpath(iw1_slc_par).as_posix(),
-                    iw1_slc,
-                    iw1_slc_par,
+                    slc_dir.joinpath(tab_names.slc).as_posix(),
+                    slc_dir.joinpath(tab_names.par).as_posix(),
+                    tab_names.slc,
+                    tab_names.par,
                     "-1.25",
                 ]
                 run_command(command, tmpdir.as_posix())
 
                 # replace iw1 slc with phase corrected files
-                shutil.move(tmpdir.joinpath(iw1_slc), slc_dir.joinpath(iw1_slc))
-                shutil.move(tmpdir.joinpath(iw1_slc_par), slc_dir.joinpath(iw1_slc_par))
+                shutil.move(tmpdir.joinpath(tab_names.slc), slc_dir.joinpath(tab_names.slc))
+                shutil.move(tmpdir.joinpath(tab_names.par), slc_dir.joinpath(tab_names.par))
 
     def mosiac_slc(self, rlks=12, alks=2):
-        self.slc = "{}_{}.slc".format(self.slc_prefix, self.polarization.upper())
-        self.slc_par = "{}_{}.slc.par".format(self.slc_prefix, self.polarization.upper())
+        slc_tab = self.slc_tab_names(self.slc_prefix)
         command = [
             "SLC_mosaic_S1_TOPS",
             self.slc_tab.as_posix(),
-            self.slc,
-            self.slc_par,
+            slc_tab.slc,
+            slc_tab.par,
             str(rlks),
-            str(alks)
+            str(alks),
         ]
         run_command(command, os.getcwd())
 
     def orbits(self):
-        command = [
-            "S1_OPOD_vec",
-            self.slc_par,
-            self.orbit_file
-        ]
+        slc_tab = self.slc_tab_names(self.slc_prefix)
+        command = ["S1_OPOD_vec", slc_tab.par, self.orbit_file]
         run_command(command, os.getcwd())
 
     def frame_subset(self):
-        self.acquisition_date = datetime.date(2018, 1, 20)
         df = pd.read_csv(self.burst_data)
-
-        df['acquistion_datetime'] = pd.to_datetime(df['acquistion_datetime'])
-        df['date'] = df['acquistion_datetime'].apply(lambda x: pd.Timestamp(x).date())
-        df_subset = df[df['date'] == self.acquisition_date]
-
+        self.acquisition_date = datetime.date(2018, 1, 8)
+        df["acquistion_datetime"] = pd.to_datetime(df["acquistion_datetime"])
+        df["date"] = df["acquistion_datetime"].apply(lambda x: pd.Timestamp(x).date())
+        df_subset = df[df["date"] == self.acquisition_date]
         tabs_param = dict()
-        for swath in [1, 2, 3]:
-            swath_df = df_subset[df_subset.swath == 'IW{}'.format(swath)]
-            swath_df.sort_values(by='acquistion_datetime', inplace=True, ascending=True)
-            tabs_param[swath]['slc'] = self.swath_slc.format(self.slc_prefix, self.polarization.upper(), swath)
-            tabs_param[swath]['par'] = self.swath_slc_par.format(
-                self.slc_prefix, self.polarization.upper(), swath
-            )
-            tabs_param[swath]['tops_par'] = self.swath_slc_tops_par.format(
-                self.slc_prefix, self.polarization.upper(), swath
-            )
+        complete_frame = True
 
-        print(tabs_param)
+        with tempfile.TemporaryDirectory() as tmpdir:
+            tmpdir = Path(tmpdir)
+            burst_tab = tmpdir.joinpath("burst_tab")
+            with open(burst_tab, "w") as fid:
+                for swath in [1, 2, 3]:
+                    tmp_dict = dict()
+                    swath_df = df_subset[df_subset.swath == "IW{}".format(swath)]
+                    swath_df = swath_df.sort_values(
+                        by="acquistion_datetime", ascending=True
+                    )
 
-if __name__ == "__main__":
-    raw_path = Path(
-        "/g/data/u46/users/pd1813/INSAR/INSAR_BACKSCATTER/test_backscatter_workflow/INSAR_ANALYSIS/VICTORIA/S1/GAMMA/raw_data/T045D/F20S"
-    )
-    scene_date = "20180108"
-    burst_data = "/g/data/u46/users/pd1813/INSAR/INSAR_BACKSCATTER/test_backscatter_workflow/INSAR_ANALYSIS/VICTORIA/S1/GAMMA/T045D_F20S/lists/slc_input.csv"
-    polarization = "vv"
-    outdir = Path(
-        "/g/data/u46/users/pd1813/INSAR/INSAR_BACKSCATTER/test_backscatter_workflow/INSAR_ANALYSIS/VICTORIA/S1/GAMMA/T045D_F20S/SLC"
-    )
-    work_dir = outdir.joinpath(scene_date)
-    work_dir.mkdir(exist_ok=True)
-    with working_directory(work_dir):
-        slc = SlcProcess(raw_path, outdir, polarization, scene_date, burst_data)
-        # slc.read_raw_data()
-        # slc.concatenate()
-        # slc.phase_shift()
-        # slc.mosiac_slc()
-        # slc.orbits()
-        slc.frame_subset()
+                    # write the burst numbers to subset from the concatenated swaths
+                    start_burst = None
+                    end_burst = None
+                    total_bursts = 0
+                    for row in swath_df.itertuples():
+                        missing_bursts = row.missing_master_bursts.strip("][")
+                        if missing_bursts:
+                            complete_frame = False
+
+                        burst_nums = [
+                            int(i) for i in row.burst_number.strip("][").split(",")
+                        ]
+                        if start_burst is None:
+                            start_burst = min(burst_nums)
+                        if end_burst is None:
+                            end_burst = max(burst_nums)
+                        else:
+                            end_burst = max(burst_nums) + total_bursts
+                        total_bursts += int(row.total_bursts)
+
+                    fid.write(str(start_burst) + " " + str(end_burst) + "\n")
+                    tab_names = self.swath_tab_names(swath, self.slc_prefix)
+                    tmp_dict["slc"] = tab_names.slc
+                    tmp_dict["par"] = tab_names.par
+                    tmp_dict["tops_par"] = tab_names.tops_par
+                    tabs_param[swath] = tmp_dict
+
+            # write out slc in and out tab files
+            sub_slc_in = tmpdir.joinpath("sub_slc_input_tab")
+            self._write_tabs(sub_slc_in, tab_params=tabs_param)
+
+            sub_slc_out = tmpdir.joinpath("sub_slc_output_tab")
+            self._write_tabs(sub_slc_out, _id=self.slc_prefix, slc_data_dir=tmpdir)
+
+            # run the subset
+            command = [
+                "SLC_copy_ScanSAR",
+                sub_slc_in.as_posix(),
+                sub_slc_out.as_posix(),
+                burst_tab.as_posix(),
+                "0",
+            ]
+            run_command(command, os.getcwd())
+
+            # replace concatenate slc with burst-subset of concatenated slc
+            for swath in [1, 2, 3]:
+                tab_names = self.swath_tab_names(swath, self.slc_prefix)
+                for item in [tab_names.slc, tab_names.par, tab_names.tops_par]:
+                    shutil.move(tmpdir.joinpath(item), item)
+
+            if not complete_frame:
+                if self.ref_master_tab is None:
+                    err = (
+                        f" ref_master_tab is None, needs ref_master_tab "
+                        f"to resize incomplete frame for scene {self.scene_date}"
+                    )
+                    raise ValueError(err)
+                self.frame_resize(self.ref_master_tab, sub_slc_in)
+
+    def frame_resize(self, ref_slc_tab: Path, full_slc_tab: Path):
+        # determine the resize burst tab
+        with tempfile.TemporaryDirectory() as tmpdir:
+            tmpdir = Path(tmpdir)
+            burst_tab = tmpdir.joinpath("burst_tab").as_posix()
+            command = [
+                "S1_BURST_tab",
+                ref_slc_tab.as_posix(),
+                full_slc_tab.as_posix(),
+                burst_tab,
+            ]
+            run_command(command, os.getcwd())
+
+            # write output in a temp directory
+            resize_slc_tab = tmpdir.joinpath("sub_slc_output_tab")
+            self._write_tabs(resize_slc_tab, _id=self.slc_prefix, slc_data_dir=tmpdir)
+
+            command = [
+                "SLC_copy_ScanSAR",
+                full_slc_tab.as_posix(),
+                resize_slc_tab.as_posix(),
+                burst_tab,
+            ]
+            run_command(command, os.getcwd())
+
+            # replace full slc with resized slc
+            for swath in [1, 2, 3]:
+                tab_names = self.swath_tab_names(swath, self.slc_prefix)
+                for item in [tab_names.slc, tab_names.par, tab_names.tops_par]:
+                    shutil.move(tmpdir.joinpath(item), item)
+
+    def burst_images(self):
+        """make a quick look of .png files for each swath and mosiac slc"""
+        def _make_png(tab_names):
+            range_samples = None
+            azimuth_lines = None
+            with open(tab_names.par, 'r') as src:
+                lines = src.readlines()
+                for line in lines:
+                    if line.startswith("range_samples:"):
+                        range_samples = int(line.split(':')[1].strip())
+                    if line.startswith("azimuth_lines:"):
+                        azimuth_lines = int(line.split(':')[1].strip())
+                    if range_samples is not None and azimuth_lines is not None:
+                        break
+
+                with tempfile.TemporaryDirectory() as tmpdir:
+                    tmpdir = Path(tmpdir)
+                    bmp_file = tmpdir.joinpath("{}".format(tab_names.slc)).with_suffix(".bmp").as_posix()
+                    command = [
+                        "rasSLC",
+                        tab_names.slc,
+                        str(range_samples),
+                        '1',
+                        str(azimuth_lines),
+                        "50",
+                        "20",
+                        "-",
+                        "-",
+                        "1",
+                        "0",
+                        "0",
+                        bmp_file
+                    ]
+                    run_command(command, os.getcwd())
+                    command = [
+                        "convert",
+                        bmp_file,
+                        Path(tab_names.slc).with_suffix(".png").as_posix()
+                    ]
+                    run_command(command, os.getcwd())
+
+        tab_names_list = [self.swath_tab_names(swath, self.slc_prefix) for swath in [1, 2, 3]]
+        tab_names_list.append(self.slc_tab_names(self.slc_prefix))
+        for tab in tab_names_list:
+            _make_png(tab)
+
+    def main(self, write_png=True):
+        """main method to execute methods need to produce slc"""
+        work_dir = self.output_dir.joinpath(self.scene_date)
+        work_dir.mkdir(exist_ok=True)
+        with working_directory(work_dir):
+            self.read_raw_data()
+            self.concatenate()
+            self.phase_shift()
+            self.mosiac_slc()
+            self.orbits()
+            self.frame_subset()
+            self.mosiac_slc()
+            if write_png:
+                self.burst_images()
