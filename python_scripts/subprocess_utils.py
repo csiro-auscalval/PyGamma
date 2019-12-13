6206c37754bb423e6d791b122bfedebf2904f27a
Passang Dorji <passang.dorji@ga.gov.au>
2019-12-13 15:59:52 +1100

    adding module to handle context manager to change luigi params and os environment

--- python_scripts/subprocess_utils.py
+++ python_scripts/subprocess_utils.py
@@ -1,18 +1,80 @@
 #!/usr/bin/python3
 
 import os
+import functools
+import itertools
+import logging
 import signal
 import subprocess
 import contextlib
 
-import logging
+
+from luigi import six
 
 _LOG = logging.getLogger(__name__)
 
 os.environ["CPL_ZIP_ENCODING"] = "UTF-8"
 
-# NOTE
-# run_command function is directly copied from https://github.com/OpenDataCubePipelines/eugl/blob/master/eugl/fmask.py
+
+class WithConfig(object):
+    """
+    Decorator to override config settings for the length of a function.
+    Usage:
+    """
+
+    def __init__(self, config, replace_sections=False):
+        self.config = config
+        self.replace_sections = replace_sections
+
+    def _make_dict(self, old_dict):
+        if self.replace_sections:
+            old_dict.update(self.config)
+            return old_dict
+
+        def get_section(sec):
+            old_sec = old_dict.get(sec, {})
+            new_sec = self.config.get(sec, {})
+            old_sec.update(new_sec)
+            return old_sec
+
+        all_sections = itertools.chain(old_dict.keys(), self.config.keys())
+        return {sec: get_section(sec) for sec in all_sections}
+
+    def __call__(self, fun):
+        @functools.wraps(fun)
+        def wrapper(*args, **kwargs):
+            import luigi.configuration
+            orig_conf = luigi.configuration.LuigiConfigParser.instance()
+            new_conf = luigi.configuration.LuigiConfigParser()
+            luigi.configuration.LuigiConfigParser._instance = new_conf
+            orig_dict = {k: dict(orig_conf.items(k)) for k in orig_conf.sections()}
+            new_dict = self._make_dict(orig_dict)
+            for (section, settings) in six.iteritems(new_dict):
+                new_conf.add_section(section)
+                for (name, value) in six.iteritems(settings):
+                    new_conf.set(section, name, value)
+            try:
+                return fun(*args, **kwargs)
+            finally:
+                luigi.configuration.LuigiConfigParser._instance = orig_conf
+        return wrapper
+
+
+@contextlib.contextmanager
+def environ(env):
+    """Temporarily set environment variables inside the context manager and
+    fully restore previous environment afterwards
+    """
+    original_env = {key: os.getenv(key) for key in env}
+    os.environ.update(env)
+    try:
+        yield
+    finally:
+        for key, value in original_env.items():
+            if value is None:
+                del os.environ[key]
+            else:
+                os.environ[key] = value
 
 
 @contextlib.contextmanager
@@ -37,6 +99,9 @@ class CommandError(RuntimeError):
 
     pass
 
+
+# NOTE
+# run_command function is directly copied from https://github.com/OpenDataCubePipelines/eugl/blob/master/eugl/fmask.py
 
 def run_command(command, work_dir, timeout=None, command_name=None):
     """
