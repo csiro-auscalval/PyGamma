20e24f68230b675a89185ac7233045e9501ac064
Passang Dorji <Passang.Dorji@ga.gov.au>
2020-01-22 08:29:46 +1100

    initial packaging script added

--- insar/scripts/package_insar.py
+++ insar/scripts/package_insar.py
@@ -1,13 +1,320 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 
 import os
-from typing import List, Optional
+from typing import Tuple, List, Optional, Union, Dict, Iterable
 from pathlib import Path
-import datetime 
-import logging 
+import datetime
+import logging
 
-from eodatasets3 import DatasetAssembler 
+import attr
+import pandas as pd
+
+import py_gamma as gamma_program
+
+from eodatasets3 import DatasetAssembler
+from eodatasets3.serialise import loads_yaml
+
+from insar.meta_data.s1_gridding_utils import generate_slc_metadata
+
 
 _LOG = logging.getLogger(__name__)
+
+
+ALIAS_FMT = {'gamma0': 'nrb_{}', 'sigma0': 'rb_{}'}
+PRODUCTS = ("sar", "insar")
+
+
+def map_product(product:str) -> Dict:
+    """Returns product names mapped to a product filename suffix."""
+    _map_dict = {
+        'sar': {
+            'suffixs': ('gamma0.tif', 'sigma0.tif'),
+            'product_base': 'SLC',
+            'product_family': 'bck',
+        },
+        'insar': {
+            'suffixs': ('unw.tif', 'int.tif', 'cc.tif'),
+            'product_base': 'INT',
+            'product_family': 'insar',
+        }
+    }
+
+    return _map_dict[product]
+
+
+def _convert_time(seconds: float, _date: datetime.date) -> datetime.datetime:
+    """Converts decimal time in seconds to datetime since epoch _date"""
+    return
+
+
+def _get_metadata(par_file: Union[Path, str]) -> Dict:
+    """
+    Returns metadata used in back  product generation.
+
+    :param par_file:
+        A full path to a parameter file used in generating backscatter product.
+
+    :returns:
+        A dict with parameters used in generating a backscatter product.
+    """
+
+    par_file = Path(par_file)
+
+    if not par_file.exists():
+        raise FileNotFoundError(f"{par_file} does not exists")
+
+    _metadata = dict()
+
+    params = gamma_program.ParFile(par_file.as_posix())
+
+    year, month, day = params.get_value("date")
+    _metadata['date'] = datetime.date(int(year), int(month), int(day))
+    _dt = datetime.datetime(int(year), int(month), int(day))
+    _metadata["center_time"] = _dt + datetime.timedelta(
+        seconds=params.get_value("center_time", dtype=float, index=0)
+    )
+    _metadata["start_time"] = _dt + datetime.timedelta(
+        seconds=params.get_value("start_time", dtype=float, index=0)
+    )
+    _metadata["end_time"] = _dt + datetime.timedelta(
+        seconds=params.get_value("end_time", dtype=float, index=0)
+    )
+
+    _metadata["incidence_angle"] = params.get_value(
+        "incidence_angle", dtype=float, index=0
+    )
+    _metadata["azimuth_angle"] = params.get_value("azimuth_angle", dtype=float, index=0)
+    _metadata["range_looks"] = params.get_value("range_looks", dtype=int, index=0)
+    _metadata["azimuth_looks"] = params.get_value("azimuth_looks", dtype=int, index=0)
+    _metadata["range_pixel_spacing"] = params.get_value(
+        "range_pixel_spacing", dtype=float, index=0
+    )
+    _metadata["azimuth_pixel_spacing"] = params.get_value(
+        "azimuth_pixel_spacing", dtype=float, index=0
+    )
+    _metadata["radar_frequency"] = params.get_value(
+        "radar_frequency", dtype=float, index=0
+    )
+    _metadata["heading"] = params.get_value("heading", dtype=float, index=0)
+    _metadata["chirp_bandwidth"] = params.get_value(
+        "chirp_bandwidth", dtype=float, index=0
+    )
+    _metadata["doppler_polynomial"] = params.get_value(
+        "doppler_polynomial", dtype=float
+    )[0:4]
+    _metadata["prf"] = params.get_value("prf", dtype=float, index=0)
+    _metadata["azimuth_proc_bandwidth"] = params.get_value(
+        "azimuth_proc_bandwidth", dtype=float, index=0
+    )
+    _metadata["receiver_gain"] = params.get_value("receiver_gain", dtype=float, index=0)
+    _metadata["calibration_gain"] = params.get_value(
+        "calibration_gain", dtype=float, index=0
+    )
+    _metadata["sar_to_earth_center"] = params.get_value(
+        "sar_to_earth_center", dtype=float, index=0
+    )
+    _metadata["earth_radius_below_sensor"] = params.get_value(
+        "earth_radius_below_sensor", dtype=float, index=0
+    )
+    _metadata["earth_semi_major_axis"] = params.get_value(
+        "earth_semi_major_axis", dtype=float, index=0
+    )
+    _metadata["earth_semi_minor_axis"] = params.get_value(
+        "earth_semi_minor_axis", dtype=float, index=0
+    )
+    _metadata["near_range_slc"] = params.get_value(
+        "near_range_slc", dtype=float, index=0
+    )
+    _metadata["center_range_slc"] = params.get_value(
+        "center_range_slc", dtype=float, index=0
+    )
+    _metadata["far_range_slc"] = params.get_value("far_range_slc", dtype=float, index=0)
+    _metadata["center_latitude"] = params.get_value(
+        "center_latitude", dtype=float, index=0
+    )
+    _metadata["center_longitude"] = params.get_value(
+        "center_longitude", dtype=float, index=0
+    )
+
+    return _metadata
+
+
+def _slc_metadata(
+    burst_data: Union[Path, str, pd.DataFrame],
+    acquisition_date: datetime.date,
+) -> Iterable[str]:
+    """
+    Returns the metdata used in forming Single-Look-Composite image.
+
+    :param burst_data:
+        A burst information data of a whole SLC stack. Either pandas
+        DataFrame or csv file.
+    :param acquisition_date:
+        A date of the acquisition.
+
+    :returns:
+        A dict with parameters used in generating SLC image file.
+    """
+    if not isinstance(burst_data, pd.DataFrame):
+        df = pd.read_csv(Path(burst_data).as_posix())
+
+    df["acquisition_datetime"] = pd.to_datetime(df["acquistion_datetime"])
+    df["date"] = df["acquisition_datetime"].apply(lambda x: pd.Timestamp(x).date())
+    df_subset = df[df["date"] == acquisition_date]
+
+    return [item for item in df_subset.url.unique()]
+
+
+def _find_products(
+    base_dir: Union[Path, str],
+    product_suffixs: Iterable[str]
+) -> List[str]:
+    """Returns List of matched suffix files from base_dir."""
+    matched_files = []
+    for item in Path(base_dir).iterdir():
+        for _suffix in product_suffixs:
+            if item.name.endswith(_suffix):
+                matched_files.append(item)
+    return matched_files
+
+
+def _write_measurement(
+    p: DatasetAssembler,
+    product_list: Iterable[Union[Path, str]],
+) -> None:
+    """
+    Unpack and package the sar and insar products
+    """
+    for product in product_list:
+        p.write_measurement(
+            name
+        )
+
+
+@attr.s(auto_attribs=True)
+class SLC:
+    """
+    A single SLC scene in a stack processing
+    """
+    track: str
+    frame: str
+    par_file: Path
+    slc_path: Path
+    slc_metadata: Dict
+
+    @classmethod
+    def for_path(
+        cls,
+        _track: str,
+        _frame: str,
+        _pol: str,
+        stack_base_path: Union[Path, str],
+        product: str,
+    ):
+
+        for slc_scene_path in Path(stack_base_path).joinpath(
+            f"{_track}_{_frame}", map_product(product)['product_base']
+        ).iterdir():
+
+            burst_data = Path(stack_base_path).joinpath(
+                f"{_track}_{_frame}",
+                f"{_track}_{_frame}_burst_data.csv"
+            )
+
+            if not burst_data.exists():
+                raise FileNotFoundError(f"{burst_data} does not exists")
+
+            par_files = [
+                item for item in slc_scene_path.glob(f"r{slc_scene_path.name}_{_pol}_*rlks.mli.par")
+            ]
+            assert len(par_files) == 1
+
+            scene_date = datetime.datetime.strptime(slc_scene_path.name, "%Y%m%d").date()
+            slc_urls = _slc_metadata(burst_data, scene_date)
+            yield cls(
+                track=_track,
+                frame=_frame,
+                par_file=par_files[0],
+                slc_path=slc_scene_path,
+                slc_metadata={
+                    Path(_url).stem: generate_slc_metadata(Path(_url)) for _url in slc_urls
+                }
+            )
+
+
+
+def _slc_attrs(doc: Dict) -> None:
+    """
+    Returns a properties common to a esa s1_slc from a doc.
+    """
+    sensor_attrs = {
+        'orbit': doc['orbit'],
+        'relative_orbit': doc['orbitNumber_rel'],
+    }
+
+    sensor = doc['sensor']
+    if sensor == 'S1A':
+        sensor_attrs['platform'] = 'Sentinel-1A'
+    elif sensor == 'S1B':
+        sensor_attrs['platform'] = 'Sentinel-1B'
+    else:
+        raise NotImplementedError(
+            f"Unexpected sensor: {sensor}, only supports S1A and S1B"
+        )
+
+    sensor_attrs['instrument'] = 'C-SAR'
+
+    return sensor_attrs
+
+
+def package(
+    track: str,
+    frame: str,
+    track_frame_base: Union[Path, str],
+    product: str,
+    out_directory: Union[Path, str],
+    polarizations: Optional[List[str]] =["VV", "VH"],
+    common_attrs: Optional[Dict] = None
+) -> None:
+
+    # Both the VV and VH polarizations has have identical SLC and burst informations.
+    # Only properties from one polarization is gathered for packaging.
+    for slc in SLC.for_path(track, frame, polarizations[0], track_frame_base, product):
+        with DatasetAssembler(
+            Path(out_directory),
+            naming_conventions="dea"
+        ) as p:
+            esa_metadata_slc = slc.slc_metadata
+            ard_slc_metadata = _get_metadata(slc.par_file)
+
+            # extract the common slc attributes from ESA SLC files
+            # subsequent slc all have the same common SLC attributes
+            if common_attrs is None:
+                for _, _meta in esa_metadata_slc.items():
+                    common_attrs = _slc_attrs(_meta['properties'])
+                    break
+            product_attrs = map_product(product)
+            p.instrument = common_attrs['instrument']
+            p.platform = common_attrs['platform']
+            p.product_family = product_attrs['product_family']
+            p.maturity = 'final'
+            p.region_code = f"{track}{frame}"
+            p.producer = "ga.gov.au"
+
+            # processed time is determined from the maketime of slc.par_file
+            # TODO better mechanism to infer the processed time of files
+            p.processed = datetime.datetime.fromtimestamp(slc.par_file.stat().st_mtime)
+            p.datetime = ard_slc_metadata['center_time']
+
+            exit()
+
+if __name__ == "__main__":
+    outdir = '/g/data/u46/users/pd1813/INSAR/INSAR_BACKSCATTER/test_backscatter_workflow'
+    par_file = "/g/data/dz56/INSAR_ARD/BACKSCATTER/T045D_F19S/SLC/20180213/r20180213_VH_4rlks.mli.par"
+    burst_data_file = (
+        "/g/data/dz56/INSAR_ARD/BACKSCATTER/T045D_F19S/T045D_F19S_burst_data.csv"
+    )
+    package("T045D", "F19S", "/g/data/dz56/INSAR_ARD/BACKSCATTER", "sar", outdir)
+
 
 
