a5184334ce7d3a2c9bd5c9beee76025ff8de94d4
Josh Sixsmith <josh.sixsmith@gmail.com>
2020-03-26 16:19:13 +1100

    Changed logger to use structlog, and output JSONL events.

--- insar/calc_multilook_values.py
+++ insar/calc_multilook_values.py
@@ -3,13 +3,14 @@
 import math
 from typing import List, Tuple, Optional, Union
 from pathlib import Path
-import logging
+import structlog
 import py_gamma as gamma_program
 
 from insar.constant import MliFilenames
 from insar.subprocess_utils import working_directory
 
-_LOG = logging.getLogger(__name__)
+# _LOG = logging.getLogger(__name__)
+_LOG = structlog.get_logger()
 
 
 def calculate_slc_look_values(slc_par_file: Union[Path, str]) -> Tuple:
