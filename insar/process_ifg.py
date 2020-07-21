87f66ccb369b383b7b4d22e24e2b97c6f373bfa1
Ben Davies <ben.davies@ga.gov.au>
2020-07-21 16:54:31 +1000

    Refactoring: move process_ifg constants to constants module.

--- insar/process_ifg.py
+++ insar/process_ifg.py
@@ -1,6 +1,7 @@
 import socket
 import structlog
 from insar.project import ProcConfig, IfgFileNames
+import insar.constant as const
 
 try:
     import py_gamma as pg
@@ -14,26 +15,6 @@ except ImportError as iex:
 
 
 _LOG = structlog.get_logger("insar")
-
-# TODO: move these to constants.py
-OFFSET_ESTIMATION_INTENSITY_CROSS_CORRELATION = 1
-OFFSET_ESTIMATION_FRINGE_VISIBILITY = 2
-
-RANGE_PATCH_SIZE = 64
-AZIMUTH_PATCH_SIZE = 64
-SLC_OVERSAMPLING_FACTOR_DEFAULT = 2
-NUM_OFFSET_ESTIMATES_RANGE = 64
-NUM_OFFSET_ESTIMATES_AZIMUTH = 256
-CROSS_CORRELATION_THRESHOLD = 0.1
-
-NON_INTERACTIVE = 0
-NOT_PROVIDED = '-'
-NO_OUTPUT = '-'
-
-# PAR1 and PAR2 parameter file type
-DIFF_PAR_OFFSET = 0  # ISP offset and interferogram parameters
-DIFF_PAR_SLC_MLI = 1  # ISP SLC/MLI parameters (default)
-DIFF_PAR_DEM = 2
 
 
 def calc_int(pc: ProcConfig,
@@ -53,10 +34,10 @@ def calc_int(pc: ProcConfig,
         stat = pg.create_offset(ic.r_master_slc_par,
                                 ic.r_slave_slc_par,
                                 ic.ifg_off,
-                                OFFSET_ESTIMATION_INTENSITY_CROSS_CORRELATION,
+                                const.OFFSET_ESTIMATION_INTENSITY_CROSS_CORRELATION,
                                 pc.range_looks,
                                 pc.azimuth_looks,
-                                NON_INTERACTIVE,
+                                const.NON_INTERACTIVE,
                                 cout=cout,
                                 cerr=cerr)
 
@@ -80,13 +61,13 @@ def calc_int(pc: ProcConfig,
                                  ic.ifg_off,  # (input) ISP offset/interferogram parameter file
                                  ic.ifg_offs,  # (output) offset estimates in range and azimuth (fcomplex)
                                  ic.ifg_ccp,  # (output) cross-correlation of each patch (0.0->1.0) (float)
-                                 RANGE_PATCH_SIZE,  # (range pixels, enter - for default from offset parameter file)
-                                 AZIMUTH_PATCH_SIZE, # (azimuth lines, enter - for default from offset parameter file)
-                                 NO_OUTPUT,  # (output) range and azimuth offsets and cross-correlation data
-                                 SLC_OVERSAMPLING_FACTOR_DEFAULT,  # (integer 2**N (1,2,4), enter - for default: 2)
-                                 NUM_OFFSET_ESTIMATES_RANGE,  # or "-" for default from offset parameter file
-                                 NUM_OFFSET_ESTIMATES_AZIMUTH,  # or "-" for default from offset parameter file
-                                 CROSS_CORRELATION_THRESHOLD,
+                                 const.RANGE_PATCH_SIZE,  # (range pixels, enter - for default from offset parameter file)
+                                 const.AZIMUTH_PATCH_SIZE, # (azimuth lines, enter - for default from offset parameter file)
+                                 const.NO_OUTPUT,  # (output) range and azimuth offsets and cross-correlation data
+                                 const.SLC_OVERSAMPLING_FACTOR_DEFAULT,  # (integer 2**N (1,2,4), enter - for default: 2)
+                                 const.NUM_OFFSET_ESTIMATES_RANGE,  # or "-" for default from offset parameter file
+                                 const.NUM_OFFSET_ESTIMATES_AZIMUTH,  # or "-" for default from offset parameter file
+                                 const.CROSS_CORRELATION_THRESHOLD,
                                  cout=cout,
                                  cerr=cerr)
             if stat:
@@ -121,10 +102,10 @@ def calc_int(pc: ProcConfig,
 
     # Create differential interferogram parameter file
     stat = pg.create_diff_par(ic.ifg_off,
-                              NOT_PROVIDED,
+                              const.NOT_PROVIDED,
                               ic.ifg_diff_par,
-                              DIFF_PAR_OFFSET,
-                              NON_INTERACTIVE,
+                              const.DIFF_PAR_OFFSET,
+                              const.NON_INTERACTIVE,
                               cout=cout,
                               cerr=cerr)
     if stat:
