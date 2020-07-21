01baccf06870171b3671d26c046c6dc6a7b99636
Ben Davies <ben.davies@ga.gov.au>
2020-07-21 16:05:06 +1000

    Refactor INT() to use config objects & reduce volume of function args.

--- insar/process_ifg.py
+++ insar/process_ifg.py
@@ -1,7 +1,15 @@
 import structlog
+from insar.project import ProcConfig, IfgFileNames
+
+# TODO: add try/except guard block for import, check hostname for to handle tests locally & gadi
 import py_gamma as pg
 
+
 _LOG = structlog.get_logger("insar")
+
+# TODO: move these to constants.py
+OFFSET_ESTIMATION_INTENSITY_CROSS_CORRELATION = 1
+OFFSET_ESTIMATION_FRINGE_VISIBILITY = 2
 
 RANGE_PATCH_SIZE = 64
 AZIMUTH_PATCH_SIZE = 64
@@ -11,69 +19,66 @@ NUM_OFFSET_ESTIMATES_AZIMUTH = 256
 CROSS_CORRELATION_THRESHOLD = 0.1
 
 NON_INTERACTIVE = 0
+NOT_PROVIDED = '-'
+NO_OUTPUT = '-'
+
+# PAR1 and PAR2 parameter file type
+DIFF_PAR_OFFSET = 0  # ISP offset and interferogram parameters
+DIFF_PAR_SLC_MLI = 1  # ISP SLC/MLI parameters (default)
+DIFF_PAR_DEM = 2
 
 
-def calc_int(ifg_off, r_master_slc, r_slave_slc,
-             r_master_slc_par, r_slave_slc_par,
-             range_looks, azimuth_looks,
-             ifg_offs, ifg_coffs, ifg_coffsets, ifg_ccp, ifg_diff_par,
-             sensor, sensor_mode, clean_up):
+def calc_int(pc: ProcConfig,
+             ic: IfgFileNames,
+             clean_up):
     """
-    TODO
-    :param ifg_off:
-    :param r_master_slc:
-    :param r_slave_slc:
-    :param r_master_slc_par:
-    :param r_slave_slc_par:
-    :param range_looks:
-    :param azimuth_looks:
-    :param ifg_offs:
-    :param ifg_coffs:
-    :param ifg_coffsets:
-    :param ifg_ccp:
-    :param ifg_diff_par:
-    :param sensor:
-    :param sensor_mode:
-    :param clean_up:
+    Perform InSAR INT processing step.
+    :param pc: ProcConfig settings obj
+    :param ic: IfgFileNames settings obj
+    :param clean_up: bool, True to delete working files after processing
     """
 
     # Calculate and refine offset between interferometric SLC pair
-    if not ifg_off.exists():
-        stat = pg.create_offset(r_master_slc_par,
-                                r_slave_slc_par,
-                                ifg_off,
-                                1,  # offset estimation algorithm:, 1=intensity cross-correlation (default)
-                                range_looks,
-                                azimuth_looks,
-                                0)  # 0=non interactive mode
-        assert stat == 0, str(stat)
-        if stat != 0:
-            print('\npg =', pg)
-            print('stat =', stat)
-            raise NotImplementedError('Handle error / stat={}'.format(stat))
+    if not ic.ifg_off.exists():
+        cout = []
+        cerr = []
+        stat = pg.create_offset(ic.r_master_slc_par,
+                                ic.r_slave_slc_par,
+                                ic.ifg_off,
+                                OFFSET_ESTIMATION_INTENSITY_CROSS_CORRELATION,
+                                pc.range_looks,
+                                pc.azimuth_looks,
+                                NON_INTERACTIVE,
+                                cout=cout,
+                                cerr=cerr)
+
+        if stat:
+            msg = "failed to execute pg.create_offset"
+            _LOG.error(msg, stat=stat, gamma_stdout=cout, gamma_stderr=cerr)
+            raise ProcessIfgException(msg)
 
         # 2-pass differential interferometry without phase unwrapping (CSK spotlight)
-        if sensor == "CSK" and sensor_mode == "SP":
-            raise NotImplementedError('Not required for sentinel 1 processing')
+        if pc.sensor == "CSK" and pc.sensor_mode == "SP":
+            raise NotImplementedError('Not required for Sentinel 1 processing')
         else:
             cout = []
             cerr = []
 
             # TODO: do any outputs need to be processed?
-            stat = pg.offset_pwr(r_master_slc,  # (input) single-look complex image 1 (reference)
-                                 r_slave_slc,  # (input) single-look complex image 2
-                                 r_master_slc_par,  # (input) SLC-1 ISP image parameter file
-                                 r_slave_slc_par,  # (input) SLC-2 ISP image parameter file
-                                 ifg_off,  # (input) ISP offset/interferogram parameter file
-                                 ifg_offs,  # (output) offset estimates in range and azimuth (fcomplex)
-                                 ifg_ccp,  # (output) cross-correlation of each patch (0.0->1.0) (float)
+            stat = pg.offset_pwr(ic.r_master_slc,  # (input) single-look complex image 1 (reference)
+                                 ic.r_slave_slc,  # (input) single-look complex image 2
+                                 ic.r_master_slc_par,  # (input) SLC-1 ISP image parameter file
+                                 ic.r_slave_slc_par,  # (input) SLC-2 ISP image parameter file
+                                 ic.ifg_off,  # (input) ISP offset/interferogram parameter file
+                                 ic.ifg_offs,  # (output) offset estimates in range and azimuth (fcomplex)
+                                 ic.ifg_ccp,  # (output) cross-correlation of each patch (0.0->1.0) (float)
                                  RANGE_PATCH_SIZE,  # (range pixels, enter - for default from offset parameter file)
                                  AZIMUTH_PATCH_SIZE, # (azimuth lines, enter - for default from offset parameter file)
-                                 "-",  # (output) range and azimuth offsets and cross-correlation data in text format, enter - for no output
+                                 NO_OUTPUT,  # (output) range and azimuth offsets and cross-correlation data
                                  SLC_OVERSAMPLING_FACTOR_DEFAULT,  # (integer 2**N (1,2,4), enter - for default: 2)
                                  NUM_OFFSET_ESTIMATES_RANGE,  # or "-" for default from offset parameter file
                                  NUM_OFFSET_ESTIMATES_AZIMUTH,  # or "-" for default from offset parameter file
-                                 CROSS_CORRELATION_THRESHOLD,  # (0.0->1.0) (or "-" for default from offset parameter file)
+                                 CROSS_CORRELATION_THRESHOLD,
                                  cout=cout,
                                  cerr=cerr)
             if stat:
@@ -83,7 +88,13 @@ def calc_int(ifg_off, r_master_slc, r_slave_slc,
 
             cout = []
             cerr = []
-            stat = pg.offset_fit(ifg_offs, ifg_ccp, ifg_off, ifg_coffs, ifg_coffsets, cout=cout, cerr=cerr)
+            stat = pg.offset_fit(ic.ifg_offs,
+                                 ic.ifg_ccp,
+                                 ic.ifg_off,
+                                 ic.ifg_coffs,
+                                 ic.ifg_coffsets,
+                                 cout=cout,
+                                 cerr=cerr)
 
             if stat:
                 msg = "failed to execute pg.offset_fit"
@@ -91,18 +102,27 @@ def calc_int(ifg_off, r_master_slc, r_slave_slc,
                 raise ProcessIfgException(msg)
 
     if clean_up:
-        for p in (ifg_offs, ifg_ccp, ifg_coffs, ifg_coffsets):
+        for p in (ic.ifg_offs, ic.ifg_ccp, ic.ifg_coffs, ic.ifg_coffsets):
             try:
                 p.unlink()
             except FileNotFoundError:
                 _LOG.error('Could not delete {}'.format(p))
 
+    cout = []
+    cerr = []
+
     # Create differential interferogram parameter file
-    pg.create_diff_par(ifg_off,
-                       "-",
-                       ifg_diff_par,
-                       0,  # PAR type 0=OFF_par, ISP offset and interferogram parameters
-                       NON_INTERACTIVE)
+    stat = pg.create_diff_par(ic.ifg_off,
+                              NOT_PROVIDED,
+                              ic.ifg_diff_par,
+                              DIFF_PAR_OFFSET,
+                              NON_INTERACTIVE,
+                              cout=cout,
+                              cerr=cerr)
+    if stat:
+        msg = "failed to execute pg.create_diff_par"
+        _LOG.error(msg, stat=stat, gamma_stdout=cout, gamma_stderr=cerr)
+        raise ProcessIfgException(msg)
 
 
 class ProcessIfgException(Exception):
--- tests/test_process_ifg.py
+++ tests/test_process_ifg.py
@@ -2,6 +2,9 @@ import sys
 import pathlib
 from unittest import mock
 
+from insar.process_ifg import ProcessIfgException
+from insar.project import ProcConfig, IfgFileNames
+
 import pytest
 
 
@@ -16,28 +19,52 @@ if PY_GAMMA_MODULE not in sys.modules:
 from insar import process_ifg  # noqa
 
 
+# FIXME: tweak settings to ensure working dir doesn't have to be changed for INT processing
+# TODO: can monkeypatch be done at higher level scope?
+# TODO: check if structlog called at least once for messages? Needs structlog mock
+
+
+def create_mocks(count, spec=None):
+    """Lazily create a bunch of like mocks (useful for large blocks of args)."""
+    return [mock.Mock(spec=spec) for _ in range(count)]
+
+
 @pytest.fixture
 def pg_int_mock():
-    """Create basic mock of the py_gamma module for the INT processing step."""
+    """Create basic mock of the py_gamma module for INT processing step."""
     pg_mock = mock.Mock()
     pg_mock.create_offset.return_value = 0
     pg_mock.offset_pwr.return_value = 0
     pg_mock.offset_fit.return_value = 0
+    pg_mock.create_diff_par.return_value = 0
     return pg_mock
 
-# FIXME: tweak settings to ensure working dir don't have to be changed for INT processing
-# TODO: check if structlog called at least once for messages? Needs structlog mock
+
+@pytest.fixture
+def pc_mock():
+    """Returns basic mock to simulate a ProcConfig object."""
+    pc = mock.Mock(spec=ProcConfig)
+    return pc
 
 
-def test_int(monkeypatch, pg_int_mock):
+@pytest.fixture
+def ic_mock():
+    """Returns basic mock to simulate an IfgFileNames object."""
+    ic = mock.Mock(spec=IfgFileNames)
+    return ic
+
+
+def test_calc_int(monkeypatch, pg_int_mock, pc_mock, ic_mock):
     """Verify default path through the INT processing step without cleanup."""
 
-    monkeypatch.setattr(process_ifg, 'pg', pg_int_mock)  # TODO: or use unittest.mock's patch? Which is better?
+    # craftily substitute the 'pg' py_gamma obj for a mock: avoids a missing import when testing
+    # locally, or calling the real thing on Gadi...
+    monkeypatch.setattr(process_ifg, 'pg', pg_int_mock)
 
-    ifg_offset = mock.Mock(spec=pathlib.Path)
-    ifg_offset.exists.return_value = False  # offset not yet processed
+    ic_mock.ifg_off = mock.Mock(spec=pathlib.Path)
+    ic_mock.ifg_off.exists.return_value = False  # offset not yet processed
 
-    process_ifg.calc_int(ifg_offset, *range(13), clean_up=False)  # use fake args for now
+    process_ifg.calc_int(pc_mock, ic_mock, clean_up=False)
 
     assert pg_int_mock.create_offset.called
 
@@ -51,28 +78,44 @@ def test_int(monkeypatch, pg_int_mock):
     assert pg_int_mock.create_diff_par.called
 
 
-def test_int_with_cleanup(monkeypatch, pg_int_mock):
+def test_calc_int_with_cleanup(monkeypatch, pg_int_mock, pc_mock, ic_mock):
     monkeypatch.setattr(process_ifg, 'pg', pg_int_mock)  # TODO: or use unittest.mock's patch? Which is better?
 
-    ifg_offset = mock.Mock(spec=pathlib.Path)
-    ifg_offset.exists.return_value = False  # offset not yet processed
+    ic_mock.ifg_off = mock.Mock(spec=pathlib.Path)
+    ic_mock.ifg_off.exists.return_value = False  # offset not yet processed
 
-    ifg_offs = mock.Mock(spec=pathlib.Path)
-    ifg_ccp = mock.Mock(spec=pathlib.Path)
-    ifg_coffs = mock.Mock(spec=pathlib.Path)
-    ifg_coffsets = mock.Mock(spec=pathlib.Path)
+    ic_mock.ifg_offs = mock.Mock(spec=pathlib.Path)
+    ic_mock.ifg_ccp = mock.Mock(spec=pathlib.Path)
+    ic_mock.ifg_coffs = mock.Mock(spec=pathlib.Path)
+    ic_mock.ifg_coffsets = mock.Mock(spec=pathlib.Path)
 
-    assert ifg_offs.unlink.called is False
-    assert ifg_ccp.unlink.called is False
-    assert ifg_coffs.unlink.called is False
-    assert ifg_coffsets.unlink.called is False
+    assert ic_mock.ifg_offs.unlink.called is False
+    assert ic_mock.ifg_ccp.unlink.called is False
+    assert ic_mock.ifg_coffs.unlink.called is False
+    assert ic_mock.ifg_coffsets.unlink.called is False
 
-    process_ifg.calc_int(ifg_offset, *range(6),
-                         ifg_offs, ifg_coffs, ifg_coffsets, ifg_ccp,
-                         "fake1", "fake2", "fake3",
-                         clean_up=True)  # use fake args for now
+    process_ifg.calc_int(pc_mock,
+                         ic_mock,
+                         clean_up=True)
 
-    assert ifg_offs.unlink.called
-    assert ifg_ccp.unlink.called
-    assert ifg_coffs.unlink.called
-    assert ifg_coffsets.unlink.called
+    assert ic_mock.ifg_offs.unlink.called
+    assert ic_mock.ifg_ccp.unlink.called
+    assert ic_mock.ifg_coffs.unlink.called
+    assert ic_mock.ifg_coffsets.unlink.called
+
+
+def test_calc_int_with_errors(monkeypatch, pg_int_mock, pc_mock, ic_mock):
+    monkeypatch.setattr(process_ifg, 'pg', pg_int_mock)
+
+    log_mock = mock.Mock()  # TODO: where's the spec for structlog's logger interface?
+    assert log_mock.error.called is False
+    monkeypatch.setattr(process_ifg, '_LOG', log_mock)
+
+    ic_mock.ifg_off = mock.Mock(spec=pathlib.Path)
+    ic_mock.ifg_off.exists.return_value = False
+    pg_int_mock.create_offset.return_value = -1
+
+    with pytest.raises(ProcessIfgException):
+        process_ifg.calc_int(pc_mock, ic_mock, clean_up=False)
+
+    assert log_mock.error.called
