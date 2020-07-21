c53fcd4be0446f884e6f7b3a20854d08c188a476
Ben Davies <ben.davies@ga.gov.au>
2020-07-21 17:05:24 +1000

    Fix structlog spec for test mock.

--- tests/test_process_ifg.py
+++ tests/test_process_ifg.py
@@ -5,12 +5,13 @@ from insar import process_ifg
 from insar.process_ifg import ProcessIfgException
 from insar.project import ProcConfig, IfgFileNames
 
+import structlog
+
 import pytest
 
 
 # FIXME: tweak settings to ensure working dir doesn't have to be changed for INT processing
 # TODO: can monkeypatch be done at higher level scope?
-# TODO: check if structlog called at least once for messages? Needs structlog mock
 
 
 def create_mocks(count, spec=None):
@@ -48,6 +49,7 @@ def test_calc_int(monkeypatch, pg_int_mock, pc_mock, ic_mock):
 
     # craftily substitute the 'pg' py_gamma obj for a mock: avoids a missing import when testing
     # locally, or calling the real thing on Gadi...
+    # TODO: monkeypatch or use unittest.mock's patch? Which is better?
     monkeypatch.setattr(process_ifg, 'pg', pg_int_mock)
 
     ic_mock.ifg_off = mock.Mock(spec=pathlib.Path)
@@ -68,7 +70,7 @@ def test_calc_int(monkeypatch, pg_int_mock, pc_mock, ic_mock):
 
 
 def test_calc_int_with_cleanup(monkeypatch, pg_int_mock, pc_mock, ic_mock):
-    monkeypatch.setattr(process_ifg, 'pg', pg_int_mock)  # TODO: or use unittest.mock's patch? Which is better?
+    monkeypatch.setattr(process_ifg, 'pg', pg_int_mock)
 
     ic_mock.ifg_off = mock.Mock(spec=pathlib.Path)
     ic_mock.ifg_off.exists.return_value = False  # offset not yet processed
@@ -96,7 +98,7 @@ def test_calc_int_with_cleanup(monkeypatch, pg_int_mock, pc_mock, ic_mock):
 def test_calc_int_with_errors(monkeypatch, pg_int_mock, pc_mock, ic_mock):
     monkeypatch.setattr(process_ifg, 'pg', pg_int_mock)
 
-    log_mock = mock.Mock()  # TODO: where's the spec for structlog's logger interface?
+    log_mock = mock.Mock(spec=structlog.stdlib.BoundLogger)  # this spec has base error(), msg() etc
     assert log_mock.error.called is False
     monkeypatch.setattr(process_ifg, '_LOG', log_mock)
 
