317f2492f36311f1af1efd39cb0a93b4520cf2ae
Ben Davies <ben.davies@ga.gov.au>
2020-08-03 17:55:33 +1000

    Add extra constants for ifg processing.

--- insar/constant.py
+++ insar/constant.py
@@ -31,6 +31,110 @@ DIFF_PAR_OFFSET = 0  # ISP offset and interferogram parameters
 DIFF_PAR_SLC_MLI = 1  # ISP SLC/MLI parameters (default)
 DIFF_PAR_DEM = 2
 
+ESTIMATION_WINDOW_RECTANGULAR = 0  # default
+ESTIMATION_WINDOW_TRIANGULAR = 1
+ESTIMATION_WINDOW_GAUSSIAN = 2
+ESTIMATION_WINDOW_NORMALIZED_VECTOR = 3
+
+RASCC_MASK_DEFAULT_COHERENCE_STARTING_LINE = 1
+RASCC_MASK_DEFAULT_INTENSITY_STARTING_LINE = 1
+RASCC_TO_EOF = 0
+N_PIXELS_DEFAULT_RANGE_AVERAGE = 1
+N_PIXELS_DEFAULT_AZIMUTH_AVERAGE = 1
+RASCC_DEFAULT_INTENSITY_THRESHOLD = 0
+
+RASCC_THINNING_THRESHOLD = 4
+
+INT_MODE_SINGLE_PASS = 0
+INT_MODE_REPEAT_PASS = 1
+
+PHASE_OFFSET_MODE_ABSOLUTE = 0
+PHASE_OFFSET_MODE_SUBTRACT_PHASE = 1
+
+RANGE_SPECTRAL_SHIFT_FLAG_APPLY_FILTER = 1
+RANGE_SPECTRAL_SHIFT_FLAG_NO_FILTER = 0
+AZIMUTH_COMMON_BAND_APPLY_FILTER = 1
+AZIMUTH_COMMON_BAND_NO_FILTER = 0
+
+DEFAULT_MINIMUM_RANGE_BANDWIDTH_FRACTION = 0.25
+
+SLC_1_RANGE_PHASE_MODE_NEAREST = 0  # aka 'rp1_flag' arg
+SLC_1_RANGE_PHASE_MODE_REF_FUNCTION_CENTRE = 1
+SLC_2_RANGE_PHASE_MODE_NEAREST = 0  # aka 'rp2_flag' arg
+SLC_2_RANGE_PHASE_MODE_REF_FUNCTION_CENTRE = 1
+
+TRIANGULATION_MODE_FILLED_TRIANGULAR_MESH = 0
+TRIANGULATION_MODE_DELAUNAY = 1
+
+INIT_FLAG_INIT_POINT_PHASE_VALUE = 0  # the default
+INIT_FLAG_SET_PHASE_0_AT_INITIAL = 1  # set phase to 0.0 at initial point
+
+BASE_INIT_METHOD_4 = 4
+
+BASE_ADD_MODE_ADD = 1
+BASE_ADD_MODE_SUBTRACT = -1
+
+
+# Phase sim constants
+PH_FLAG_SIMULATED_UNFLATTENED_INTERFEROGRAM = 0
+PH_FLAG_SIMULATED_FLATTENED_INTERFEROGRAM = 1
+B_FLAG_INIT_BASELINE = 0
+B_FLAG_PRECISION_BASELINE = 1
+PH_MODE_ABSOLUTE_PHASE = 0
+PH_MODE_SUBTRACT_PHASE_OFFSET = 1
+
+# interp_ad weighting mode:
+# 0:constant
+# 1: 1-(r/r_max)   default(-): 1-(r/r_max)
+# 2: 1-(r/r_max)**2
+# 3: exp(-2.*SQR(r/r_max))
+# TODO: get good names for these from InSAR team
+WEIGHTING_MODE_CONSTANT = 0
+WEIGHTING_MODE_1 = 1
+WEIGHTING_MODE_2 = 2
+WEIGHTING_MODE_3 = 3
+
+# Geocoding constants
+# interpolation mode (enter - for default)
+INTERPOLATION_MODE_NEAREST_NEIGHBOR = 0
+INTERPOLATION_MODE_BICUBIC_SPLINE = 1  # (default)
+INTERPOLATION_MODE_BICUBIC_SPLINE_INTERP_LOG = 2  # bicubic-spline, interpolate log(data)
+INTERPOLATION_MODE_BICUBIC_SPLINE_INTERP_SQRT = (
+    3  # bicubic-spline, interpolate sqrt(data)
+)
+#   4: B-spline interpolation (default B-spline degree: 5)
+#   5: B-spline interpolation sqrt(x) (default B-spline degree: 5)
+#   6: Lanczos interpolation (default Lanczos function order: 5)
+#   7: Lanczos interpolation sqrt(x) (default Lanczos function order: 5)
+
+# Data types
+DTYPE_FLOAT = 0  # (default)
+DTYPE_FCOMPLEX = 1
+#    2: SUN raster/BMP/TIFF format
+DTYPE_UNSIGNED_CHAR = 3
+DTYPE_SHORT = 4
+DTYPE_DOUBLE = 5
+
+# GEOCODE constants
+DEFAULT_STARTING_LINE = 1
+DISPLAY_TO_EOF = 0
+
+DTYPE_GEOTIFF_FLOAT = 2
+
+# file patterns to delete after geocoding step
+TEMP_FILE_GLOBS = [
+    "*flat0*",
+    "*flat1*",
+    "*sim0*",
+    "*sim1*",
+    "*int1*",
+    "*.ras",
+    "*.unw",
+    "*eqa.int",
+    "*filt.int",
+    "*.cc",
+]
+
 
 class SlcFilenames(Enum):
     """SLC filenames """
