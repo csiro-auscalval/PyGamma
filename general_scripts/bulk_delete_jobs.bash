4396322741d1bfe649de3b140ea00e0c3c1ef032
Alistair Deane <Alistair.Deane@ga.gov.au>
2020-12-16 13:47:01 +1100

    increased string length because NCI job IDs have incremented to 8 digits

--- general_scripts/bulk_delete_jobs.bash
+++ general_scripts/bulk_delete_jobs.bash
@@ -18,7 +18,7 @@ if [ -z $jobs ]; then
     echo ""
     qstat > list1 #create list of jobs
     grep -wE "($user)" list1 > list2 #extract jobs for user
-    cut -c-7 list2 > list3 #extract job numbers
+    cut -c-8 list2 > list3 #extract job numbers
     # delete jobs
     while read job; do
 	qdel $job
