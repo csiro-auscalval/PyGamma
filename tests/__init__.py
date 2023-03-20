2d0780421311b6f647a35b6c0f044ba1385fd6c2
Dale Roberts <dale.o.roberts@gmail.com>
2023-03-20 15:28:54 +1100

    Check if GAMMA_VER is set when running tests, if it is not then set a default and warn.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,13 +1,24 @@
 """Unit test package for insar."""
 
+import atexit
 import sys
+import os
+
 from unittest import mock
 
 
 PY_GAMMA_MODULE = "py_gamma"
 
 if PY_GAMMA_MODULE not in sys.modules:
+    try:
+        os.environ['GAMMA_VER']
+    except KeyError:
+        os.environ['GAMMA_VER'] = '20191203'
+        def warn():
+            print(f"WARNING: Environment variable GAMMA_VER was not set so it was defaulted to GAMMA_VER={os.environ['GAMMA_VER']} for mocking GAMMA and running tests.")
+        if sys.stdout != sys.__stdout__:
+            atexit.register(warn)
+        
     # mock Gamma for local testing
     py_gamma_mock = mock.Mock()
-    msg = "This is a mock py_gamma, Gamma not found/installed on this platform"
     sys.modules[PY_GAMMA_MODULE] = py_gamma_mock
