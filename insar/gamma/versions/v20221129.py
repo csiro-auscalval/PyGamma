f6103e53701ed56cb3b951b03a01d33fca5d94a1
Dale Roberts <dale.o.roberts@gmail.com>
2023-05-12 20:19:23 +1000

    Cleanup logging in v20221129

--- insar/gamma/versions/v20221129.py
+++ insar/gamma/versions/v20221129.py
@@ -13,17 +13,11 @@ class PyGammaProxy(PyGammaProxy_v20211208):
     def create_offset(self, SLC1_par: str, SLC2_par: str, OFF_par: str, algorithm = None, rlks = None, azlks = None, iflg = 0):
         supplied_args = self._clean_call_args(locals(), inspect.signature(self.create_offset))
 
-        STATUS_LOGGER.debug(f"signature: {inspect.signature(self.create_offset)}")
-
-        STATUS_LOGGER.debug(f"create_offset: {supplied_args}")
-
         if self.validate_inputs:
             self._validate_create_offset(*supplied_args)
 
         if self.mock_outputs:
             self._mock_create_offset_outputs(*supplied_args)
-
-        STATUS_LOGGER.debug(f"create_offset: {supplied_args}")
 
         return self._gamma_call("ISP", "create_offset", supplied_args)
 
