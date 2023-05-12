d7b175b3a29474a5ef0a1f5bc848a76f5834ef09
Dale Roberts <dale.o.roberts@gmail.com>
2023-05-12 20:37:47 +1000

    Add logging to process_utils

--- insar/process_utils.py
+++ insar/process_utils.py
@@ -1,9 +1,11 @@
 import numpy as np
+
 from PIL import Image
 from pathlib import Path
 from typing import Union, Optional
 
 from insar.path_util import append_suffix
+from insar.logs import STATUS_LOGGER as LOG
 
 def convert(
     input_file: Union[Path, str],
@@ -30,11 +32,12 @@ def convert(
     if not output_file:
         output_file = append_suffix(Path(input_file.stem), ".png")
 
-    # Convert the bitmap to a PNG w/ black pixels made transparent
+    LOG.debug(f"Converting bitmap {input_file} to {output_file}")
     img = Image.open(input_file)
     img = np.array(img.convert('RGBA'))
 
     if black_to_transparent:
+        LOG.debug(f"Converting black pixels to transparent pixels in {output_file}")
         img[(img[:, :, :3] == (0, 0, 0)).all(axis=-1)] = (0, 0, 0, 0)
 
     Image.fromarray(img).save(output_file)
