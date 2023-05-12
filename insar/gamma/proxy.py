01c946cfbd616ca6e200770ebc8599f7b963a91a
Dale Roberts <dale.o.roberts@gmail.com>
2023-05-12 20:18:34 +1000

    Fix up typing in gamma.proxy

--- insar/gamma/proxy.py
+++ insar/gamma/proxy.py
@@ -2,11 +2,13 @@ import insar.logs as logs
 import importlib
 import os
 
+from typing import Type, cast
 from insar.logs import GAMMA_LOGGER as LOG
 from insar.py_gamma_ga import GammaInterface, auto_logging_decorator, subprocess_wrapper
+from insar.gamma.generated import PyGammaProxy
 
 
-def get_gamma_version():
+def get_gamma_version() -> str:
     """
     Get the version of GAMMA being used by the `PyGamma` workflow.
 
@@ -15,15 +17,13 @@ def get_gamma_version():
 
     See `configs/activate.env` for details.
     """
-
     version = os.environ.get("GAMMA_VER")
     if not version:
         raise RuntimeError("Failed to detect GAMMA version (expected 'GAMMA_VER' env var)")
-
     return version
 
 
-def create_versioned_gamma_proxy(gamma_ver: str, base_wrapper: object, exception_type: BaseException):
+def create_versioned_gamma_proxy(gamma_ver: str, base_wrapper: object, exception_type: Type[Exception]) -> PyGammaProxy:
     """
     Creates a GAMMA proxy object for a specific version of GAMMA, which translates from the API for
     GAMMA version 20191203 into the user specified GAMMA version.
@@ -33,17 +33,13 @@ def create_versioned_gamma_proxy(gamma_ver: str, base_wrapper: object, exception
     layers / proxy objects.
     """
     wrapper_module = importlib.import_module(f"insar.gamma.versions.v{gamma_ver}")
+    return cast(PyGammaProxy, wrapper_module.PyGammaProxy(exception_type, base_wrapper))
 
-    return wrapper_module.PyGammaProxy(exception_type, base_wrapper)
 
-
-def create_gamma_proxy(exception_type: BaseException):
+def create_gamma_proxy(exception_type: Type[Exception]) -> PyGammaProxy:
     """
     A convenience function for `create_versioned_gamma_proxy` that creates a proxy object for the
     currently installed version of GAMMA that wraps the `insar.py_gamma_ga` GAMMA call dispatch interface.
     """
-    pg = GammaInterface(
-        subprocess_func=auto_logging_decorator(subprocess_wrapper, exception_type, LOG)
-    )
-
+    pg = GammaInterface(subprocess_func=auto_logging_decorator(subprocess_wrapper, exception_type, LOG))
     return create_versioned_gamma_proxy(get_gamma_version(), pg, exception_type)
