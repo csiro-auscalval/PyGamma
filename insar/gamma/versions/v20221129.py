08f4bbb7889aeec272dacb4c3c43cb09a73adeef
Dale Roberts <dale.o.roberts@gmail.com>
2023-05-18 12:26:42 +1000

    Fix all the types in py_gamma_proxy

--- insar/gamma/versions/v20221129.py
+++ insar/gamma/versions/v20221129.py
@@ -10,26 +10,18 @@ print(f"Interfacing with GAMMA Proxy {__file__}")
 
 class PyGammaProxy(PyGammaProxy_v20211208):
 
-    def create_offset(self, SLC1_par: str, SLC2_par: str, OFF_par: str, algorithm = None, rlks = None, azlks = None, iflg = 0):
-        supplied_args = self._clean_call_args(locals(), inspect.signature(self.create_offset))
-
-        if self.validate_inputs:
-            self._validate_create_offset(*supplied_args)
-
-        if self.mock_outputs:
-            self._mock_create_offset_outputs(*supplied_args)
-
-        return self._gamma_call("ISP", "create_offset", supplied_args)
+#    def create_offset(self, SLC1_par: str, SLC2_par: str, OFF_par: str, algorithm = None, rlks = None, azlks = None, iflg = 0):
+#        supplied_args = self._clean_call_args(locals(), inspect.signature(self.create_offset))
+#
+#        if self.validate_inputs:
+#            self._validate_create_offset(*supplied_args)
+#
+#        if self.mock_outputs:
+#            self._mock_create_offset_outputs(*supplied_args)
+#
+#        return self._gamma_call("ISP", "create_offset", supplied_args)
 
     def SLC_mosaic_S1_TOPS(self, SLC_tab: str, SLC: str, SLC_par: str, rlks, azlks, bflg = None, SLCR_tab: str = None):
         supplied_args = self._clean_call_args(locals(), inspect.signature(self.SLC_mosaic_S1_TOPS))
-
-        if self.validate_inputs:
-            self._validate_SLC_mosaic_S1_TOPS(*supplied_args)
-
-        if self.mock_outputs:
-            self._mock_SLC_mosaic_S1_TOPS_outputs(*supplied_args)
-
-        # replacement of binary by `SLC_mosaic_ScanSAR` in this version
 
         return self._gamma_call("ISP", "SLC_mosaic_ScanSAR", supplied_args)
